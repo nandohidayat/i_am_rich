43314a18efa0c190877ca8b81db909bb78261de8
nandohidayat <mnandohidayat@gmail.com>
2019-06-22 13:47:18 +0700

    add app icon

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("I Am Ric"),
+          title: Text("I Am Rich"),
           backgroundColor: Colors.blueGrey.shade900,
         ),
         body: Center(
